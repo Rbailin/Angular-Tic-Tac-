72a00d446a59d8eb6996ff225fcd1c033bd9c33e
RachelBailin <rachelebailin@gmail.com>
2014-01-20 18:52:42 -0800

    set images

--- main.js
+++ main.js
@@ -5,33 +5,21 @@ function playerTurn ($scope) {
 	$scope.boxes = [['','',''],['','',''], ['','','']];
 	var xTurn = {var: false};
 	var gameover = false;
+	var X = "skatedog.jpeg";
+	var O = "UndieSkate.jpg";
+	
 
 	$scope.clickhere = function(r,c) {
-
-		if ($scope.winner == 'X' || $scope.winner == 'O') {
+		if($scope.winner == X || $scope.winner == O){
 			return gameover = true;
 		}
-
-		if($scope.boxes[r][c] == "" && !gameover) {
+			if($scope.boxes[r][c] != X || $scope.boxes[r][c] != O && !gameover) {
 			if(xTurn.var = !xTurn.var)
-				$scope.boxes[r][c] = "X";
+				$scope.boxes[r][c] = X;
 			else
-				$scope.boxes[r][c] = "O";
+				$scope.boxes[r][c] = O;
 		}
-		
-		
 	}
-
-
-	// $scope.clickhere=function (r,c) {
-	// 	// alert(r + " " + c);
-	// 	$scope.boxes[r][c]=
-	// 	($scope.boxes[r][c]==''? 
-	// 		((xTurn.val=!xTurn.val) ? 'X' : 'O') : 
-	// 		$scope.boxes[r][c]);
-		
-	// }
-	// winningPlayer();
 
 	$scope.winningPlayer = function () {	
 
@@ -44,58 +32,60 @@ function playerTurn ($scope) {
 
 			for(b=0; b<3; b++){
 				
-				if($scope.boxes[a][b] == 'X') {rowx++} 
+				if($scope.boxes[a][b] == X) {rowx++} 
 				
-				if($scope.boxes[b][a] == 'X') {columnx++} 
+				if($scope.boxes[b][a] == X) {columnx++} 
 				
-				if($scope.boxes[b][b] == 'X') {dia1x++}
+				if($scope.boxes[b][b] == X) {dia1x++}
 
-				if($scope.boxes[b][2-b] == 'X') {dia2x++}
+				if($scope.boxes[b][2-b] == X) {dia2x++}
 
 				
-				if($scope.boxes[a][b] == 'O') {rowo++}
+				if($scope.boxes[a][b] == O) {rowo++}
 
-				if($scope.boxes[a][b] == 'O') {columno++}
+				if($scope.boxes[a][b] == O) {columno++}
 				
-				if($scope.boxes[a][b] == 'O') {dia1o++}
+				if($scope.boxes[a][b] == O) {dia1o++}
 				
-				if($scope.boxes[a][b] == 'O') {dia2o++} 
+				if($scope.boxes[b][2-b] == O) {dia2o++} 
 			};
 
 		
 			if(rowx == 3) 
-				$scope.winner = 'X';
+				$scope.winner = X;
 			if(columnx == 3) 
-				$scope.winner = 'X';
+				$scope.winner = X;
 			if(dia1x == 3) 
-				$scope.winner = 'X';
+				$scope.winner = X;
 			if(dia2x == 3) 
-				$scope.winner = 'X';
+				$scope.winner = X;
 			if(rowo == 3) 
-				$scope.winner = 'O';
+				$scope.winner = O;
 			if(columno == 3) 
-				$scope.winner = 'O';
+				$scope.winner = O;
 			if(dia1o == 3) 
-				$scope.winner = 'O';
+				$scope.winner = O;
 			if(dia2o == 3) 
-				$scope.winner = 'O';
+				$scope.winner = O;
 		};	
 
 			
 	};
 
-	$scope.reset = function () {
-		console.log("Before: " +  $scope.boxes);
-		for (var row = 0; row < $scope.boxes.length; row++) {
-			console.log("row: " + row);
-			for (var cell = 0; cell < $scope.boxes.length; cell++) {
-				$scope.boxes[row][cell] = "";
-				console.log("cell: " + cell);
-			};
-		};
-		
+	// $scope.reset = function () {
+	// 	console.log("Before: " +  $scope.boxes);
+	// 	for (var row = 0; row < $scope.boxes.length; row++) {
+	// 		console.log("row: " + row);
+	// 		for (var cell = 0; cell < $scope.boxes.length; cell++) {
+	// 			$scope.boxes[row][cell] = null;
+	// 			console.log("cell: " + cell);
+	// 		};
+	// 	};	
+	// };
 
-	};
+	$scope.reset = function() {
+		$scope.boxes = [['','',''],['','',''], ['','','']];
+	}	
 };
 
 
